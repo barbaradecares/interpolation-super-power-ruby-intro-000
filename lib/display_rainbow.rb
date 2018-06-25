06a1062cda5f7a28a13edab3685042b55ba59e2d
Barbara Decares <barbaradecares@hotmail.com>
2018-06-25 18:42:02 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
-
+colors=[]
 def display_rainbow (colors)
 
 puts "#{colors[0].slice(0, 1)}: #{colors[0]} , #{colors[1].slice(0, 1).uppercase}: #{colors[1]}, #{colors[2].slice(0, 1).uppercase}: #{colors[2]}, #{colors[3].slice(0, 1).uppercase}: #{colors[3]} , #{colors[4].slice(0, 1).uppercase}: #{colors[4]}, #{colors[5].slice(0, 1).uppercase}: #{colors[5]}, #{colors[6].slice(0,1).uppercase}: #{colors[6]}"
